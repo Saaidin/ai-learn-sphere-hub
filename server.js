4b88e6f4839fa539512e07208383c97a579fa91d
saaidin <dinme73@gmail.com>
2025-04-10 00:51:18 +0800

    update server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const { Pool } = pkg;
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.get('/', (req, res) => {
+  res.send('Affiliate API is running');
+});
+
 app.use(cors());
 app.use(express.json());
 
