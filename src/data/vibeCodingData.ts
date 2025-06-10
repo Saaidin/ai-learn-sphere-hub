e458f1c0758e06627608c159e66da7736765e968
saaidin <dinme73@gmail.com>
2025-06-10 18:41:27 +0800

    add vibe coding for new online course platform app

--- src/data/vibeCodingData.ts
+++ src/data/vibeCodingData.ts
@@ -1,15 +1,25 @@
 export const vibeCodingItems = [
   {
-    id: '1',
-    coverImage: '/images/web-img/wc-1.jpg',
-    title: 'AI-Amazona: Ecommerce Website',
+    id: '4',
+    coverImage: '/images/vibe-code/vc-7.png',
+    title: 'Online Course Platform',
     author: 'Saaidin Mat Esa',
-    description: 'Ketahui cara membuat tapak web yang digunakan masakini dengan bantuan alatan AI. Saya ingin kongsikan pengetahuan asas untuk membantu Vibe Coder yang baru.',
-    downloadLink: 'https://designrr.s3.amazonaws.com/dinme73_at_gmail.com_150555/how-to-build-website_68049aaa.pdf',
-    category: 'Web Development',
-    pages: 50,
+    description: 'Anda boleh mengikut course secara percuma. Ini adalah cuma contoh projek di mana saya akan membuat course yang sebenar di masa depan.',
+    category: 'App Development',
+    pages: 1,
     freePreview: true,
-    websiteUrl: 'https://amazona-eobzbc2cz-saaidins-projects.vercel.app/'
+    websiteUrl: 'https://online-course-platform-umber.vercel.app'
+  },
+  {
+    id: '3',
+    coverImage: '/images/vibe-code/vc-3.png',
+    title: 'Recipe Idea Generator',
+    author: 'Saaidin Mat Esa',
+    description: 'Anda boleh mencari idea resipi dengan mudah menggunakan aplikasi ini. Ia menggunakan AI untuk mencadangkan resipi berdasarkan bahan yang anda ada.',
+    category: 'App Development',
+    pages: 1,
+    freePreview: true,
+    websiteUrl: 'https://aistudio.google.com/app/apps/drive/10L0a8z0Ott4tLzg1PFOFOGl2VBFaZs0Y?showPreview=true'
   },
   {
     id: '2',
@@ -23,15 +33,15 @@ export const vibeCodingItems = [
     websiteUrl: 'https://shopcart-teal.vercel.app'
   },
   {
-    id: '3',
-    coverImage: '/images/vibe-code/vc-3.png',
-    title: 'Recipe Idea Generator',
+    id: '1',
+    coverImage: '/images/web-img/wc-1.jpg',
+    title: 'AI-Amazona: Ecommerce Website',
     author: 'Saaidin Mat Esa',
-    description: 'Anda boleh mencari idea resipi dengan mudah menggunakan aplikasi ini. Ia menggunakan AI untuk mencadangkan resipi berdasarkan bahan yang anda ada.',
-    category: 'App Development',
-    pages: 1,
+    description: 'Ketahui cara membuat tapak web yang digunakan masakini dengan bantuan alatan AI. Saya ingin kongsikan pengetahuan asas untuk membantu Vibe Coder yang baru.',
+    downloadLink: 'https://designrr.s3.amazonaws.com/dinme73_at_gmail.com_150555/how-to-build-website_68049aaa.pdf',
+    category: 'Web Development',
+    pages: 50,
     freePreview: true,
-    websiteUrl: 'https://aistudio.google.com/app/apps/drive/10L0a8z0Ott4tLzg1PFOFOGl2VBFaZs0Y?showPreview=true'
+    websiteUrl: 'https://amazona-eobzbc2cz-saaidins-projects.vercel.app/'
   }
-  
 ];
